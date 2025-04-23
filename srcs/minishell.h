547f6e08c0ffad6c1d0108453da99d1787adb02d
laher_maciel <laherwpayotmaciel@gmail.com>
2025-04-23 15:16:18 +0100

    added makefile and my base libft.

---
+++ srcs/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahermaciel <lahermaciel@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/23 15:05:39 by lahermaciel       #+#    #+#             */
+/*   Updated: 2025/04/23 15:11:33 by lahermaciel      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <errno.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <string.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <termios.h>
+
+#endif
