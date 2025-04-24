714bcea996e7661e146d0e2ee9f5a58c938fd8ca
laher_maciel <laherwpayotmaciel@gmail.com>
2025-04-24 16:51:18 +0100

    Now it receives and prints the input of the terminal.

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,33 @@
 /*   By: lahermaciel <lahermaciel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 15:07:03 by lahermaciel       #+#    #+#             */
-/*   Updated: 2025/04/24 16:19:15 by lahermaciel      ###   ########.fr       */
+/*   Updated: 2025/04/24 16:50:10 by lahermaciel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+void	receive_input(void)
 {
 	char	*line;
 
-	line = readline(">>");
-	ft_printf("line = %s\n", line);
+	line = NULL;
+	while (1)
+	{
+		line = readline(">> ");
+		ft_printf("%s\n", line);
+		if (!ft_strncmp(line, "exit", ft_strlen(line)))
+		{
+			free(line);
+			return ;
+		}
+		if (line)
+			free(line);
+	}
+}
+
+int	main(void)
+{
+	receive_input();
 	return (0);
 }
